4846c199184fc152d75dd0df650a28aac1deb277
Yvens16 <yvensbelaston@gmail.com>
2023-04-01 11:10:23 +0200

    pourquoi invalider le token dans BlackListTokenEntity

--- src/main/java/com/jwt_back17/jwt_back/entity/BlackListTokenEntity.java
+++ src/main/java/com/jwt_back17/jwt_back/entity/BlackListTokenEntity.java
@@ -5,6 +5,15 @@ import javax.persistence.Id;
 
 @Entity
 public class BlackListTokenEntity {
+  /**
+   * Avoir un système de blacklist permet d'invalider un token au moment ou
+   * l'utilisateur se déconnecte
+   * Et oui, le fait de se déconnecter en lui même permettrais à une personne
+   * malveilllante de toujours utiliser un précédent token
+   * Tant que la date d'expiration n'est pas dépassé
+   * Et en cas d'alerte l'utilisateur ou le sytème peut invalider un token jsute
+   * en se déconnectant
+   */
 
   @Id
   private String token;
